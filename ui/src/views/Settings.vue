6d95cef65387f81fee63d91a1d24a34ea51e9949
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-05-21 22:44:44 +0200

    Remove advanced options

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -62,13 +62,13 @@
               }}</template>
             </cv-toggle>
               <!-- advanced options -->
-            <cv-accordion ref="accordion" class="maxwidth mg-bottom">
+            <!--<cv-accordion ref="accordion" class="maxwidth mg-bottom">
               <cv-accordion-item :open="toggleAccordion[0]">
                 <template slot="title">{{ $t("settings.advanced") }}</template>
                 <template slot="content">
                 </template>
               </cv-accordion-item>
-            </cv-accordion>
+            </cv-accordion> -->
             <cv-row v-if="error.configureModule">
               <cv-column>
                 <NsInlineNotification
