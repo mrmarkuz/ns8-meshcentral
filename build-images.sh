d6775ce7d2fd44e2b37482a18107de1060ae608f
mrmarkuz <mrmarkuz666@gmail.com>
2026-05-28 14:41:11 +0200

    Raise mongo version

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mongo:7.0.30 ghcr.io/ylianst/meshcentral:1.2.0-mongodb" \
+    --label="org.nethserver.images=docker.io/mongo:7.0.34 ghcr.io/ylianst/meshcentral:1.2.0-mongodb" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
